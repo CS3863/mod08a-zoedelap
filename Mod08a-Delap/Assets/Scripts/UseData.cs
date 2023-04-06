2da78ec77153bd7c203572d57227bb9f2436a2eb
zoedelap <zoe.a.delap@gmail.com>
2023-04-06 13:34:51 -0500

    broken code that will drive me nuts. but the other stuff works. so that's good.

--- Mod08a-Delap/Assets/Scripts/UseData.cs
+++ Mod08a-Delap/Assets/Scripts/UseData.cs
@@ -20,8 +20,12 @@ public class UseData : MonoBehaviour
     private object tempObj;
     private float tempFloat;
 
+    // private Renderer objRenderer;
+
     void Awake()
     {
+        // objRenderer = gameObject.GetComponent<Renderer>();
+
         data = CSVReader.Read("narrow_co2");
 
         rowMax = data.Count;
@@ -41,11 +45,19 @@ public class UseData : MonoBehaviour
         if (rowCount < rowMax) {
             // get and convert the value, increment rowCount for the next iteration
             tempObj = data[rowCount]["xco2"];
-            tempFloat = System.Convert.ToSingle(tempObj)-350;
-            Debug.Log("current tempFloat: " + tempFloat + ", current row number: " + rowCount);
+            tempFloat = System.Convert.ToSingle(tempObj);
+            // Debug.Log("current tempFloat: " + tempFloat + ", current row number: " + rowCount);
             rowCount++;
 
+            // // update the color of the object
+            // float colorValue = (tempFloat - 388) / (427 - 388);
+            // Debug.Log("color value: " + colorValue + ", " + 0.0f + ", " + 0.02745f);
+            // Color color = new Color(colorValue, 0.0f, 0.02745f, 1.0f);
+            // Color color = Color.grey;
+            // objRenderer.material.SetColor("_Color", color);
+            
             // update the size of the item that this is attached to 
+            tempFloat = (tempFloat - 350) * 5;
             transform.localScale = new Vector3(tempFloat, tempFloat, tempFloat);
         }
     }
