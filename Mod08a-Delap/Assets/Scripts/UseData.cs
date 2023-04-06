a748bf904dc5c5fa118b9ba059842177fc94d8fe
zoedelap <zoe.a.delap@gmail.com>
2023-04-06 13:02:18 -0500

    functional useData to resize box for each entry

--- Mod08a-Delap/Assets/Scripts/UseData.cs
+++ Mod08a-Delap/Assets/Scripts/UseData.cs
@@ -3,21 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class UseData : MonoBehaviour
-{/**
-  * Tutorial link
-  * https://github.com/tikonen/blog/tree/master/csvreader
-  * */
+{
+    /**
+    * Tutorial link
+    * https://github.com/tikonen/blog/tree/master/csvreader
+    * */
 
     List<Dictionary<string, object>> data; 
-    
-    int rowNum = 0;
 
-    private int startDelay = 1.0f;
-    private int timeInterval = 0.5f;
+    private float startDelay = 1.0f;
+    private float timeInterval = 0.5f;
+
+    private int rowCount = 0;
+    private int rowMax;
+
+    private object tempObj;
+    private float tempFloat;
 
     void Awake()
     {
         data = CSVReader.Read("narrow_co2");
+
+        rowMax = data.Count;
 
         for (var i = 0; i < data.Count; i++)
         {
@@ -31,6 +38,15 @@ public class UseData : MonoBehaviour
     }
 
     void modifyObj() {
+        if (rowCount < rowMax) {
+            // get and convert the value, increment rowCount for the next iteration
+            tempObj = data[rowCount]["xco2"];
+            tempFloat = System.Convert.ToSingle(tempObj);
+            Debug.Log("current tempFloat: " + tempFloat);
+            rowCount++;
 
+            // update the size of the item that this is attached to 
+            transform.localScale = new Vector3(tempFloat, tempFloat, tempFloat);
+        }
     }
 }
