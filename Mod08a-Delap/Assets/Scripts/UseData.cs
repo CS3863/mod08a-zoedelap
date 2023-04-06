ef3229f9de6ee34cf7b4a358b223fb5cdb402533
zoedelap <zoe.a.delap@gmail.com>
2023-04-06 12:48:52 -0500

    back to basics

--- Mod08a-Delap/Assets/Scripts/UseData.cs
+++ Mod08a-Delap/Assets/Scripts/UseData.cs
@@ -9,15 +9,15 @@ public class UseData : MonoBehaviour
   * */
 
     List<Dictionary<string, object>> data; 
-    public GameObject myCube; //prefab
-    int cubeCount; //variable 
+    
+    int rowNum = 0;
 
-    private float startDelay = 2.0f;
-    private float timeInterval = 0.5f;
+    private int startDelay = 1.0f;
+    private int timeInterval = 0.5f;
 
     void Awake()
     {
-        data = CSVReader.Read("narrow_co2");//udata is the name of the csv file 
+        data = CSVReader.Read("narrow_co2");
 
         for (var i = 0; i < data.Count; i++)
         {
@@ -27,33 +27,10 @@ public class UseData : MonoBehaviour
 
     void Start()
     {
-        for (var i = 0; i < data.Count; i++)
-        {
-            object xco2 = data[i]["xco2"]; //get age data
-            cubeCount += (int)xco2; //convert age data to int and add to cubeCount
-            Debug.Log("cubeCount " + cubeCount);
-        }
-
         InvokeRepeating("spawnObj", startDelay, timeInterval);
-
-    }
-
-    void Update()
-    {
-        //As long as cube count is not zero, instantiate prefab
-        // while (cubeCount > 0)
-        // {
-        //     Instantiate(myCube);
-        //     cubeCount--;
-        //     Debug.Log("cubeCount " + cubeCount);
-        // }
     }
 
     void spawnObj() {
-        if (cubeCount > 0) {
-            Instantiate(myCube);
-            cubeCount--;
-            Debug.Log("cubeCount" + cubeCount);
-        }
+        
     }
 }
